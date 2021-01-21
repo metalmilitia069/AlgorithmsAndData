e2330b4357d8f3ad0d6b424a28c2baab0a37f6c4
leo <medina.engineering@outlook.com>
2021-01-21 09:25:18 -0300

    Commit 6

---
+++ ConsoleSolution2Algorithms/TreeNode.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleSolution2Algorithms
+{
+    class TreeNode<T> where T : IComparable<T>
+    {
+        public T Value { get; set; }
+        public TreeNode<T> Right { get; set; }
+        public TreeNode<T> Left { get; set; }
+
+        public TreeNode(T value)
+        {
+            Value = value;
+        }
+
+        public void Insert(T newvalue)
+        {
+            int compare = newvalue.CompareTo(Value);
+            if (compare==0)
+            {
+                return;
+            }
+            if (compare<0)
+            {
+                if (Left == null)
+                {
+                    Left = new TreeNode<T>(newvalue);
+                }
+                else
+                {
+                    Left.Insert(newvalue);
+                }                
+            }
+            else
+            {
+                if (Right == null)
+                {
+                    Right = new TreeNode<T>(newvalue);
+                }
+                else
+                {
+                    Right.Insert(newvalue);
+                }
+            }
+        }
+
+        public TreeNode<T> Get(T value)
+        {
+            int compare = value.CompareTo(Value);
+
+            if (compare == 0)
+            {
+                return this;
+            }
+            if (compare < 0)
+            {
+                if (Left != null)
+                {
+                    return Left.Get(value);
+                }
+            }
+            else
+            {
+                if (Right != null)
+                {
+                    return Right.Get(value);
+                }
+            }
+
+            return null;
+        }
+
+    }
+}
